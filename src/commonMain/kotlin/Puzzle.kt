9696533beb986a65b292d8ee68f6c44d71a615c7
Guthrie McAfee Armstrong <git@gmarmstrong.dev>
2022-08-31 15:56:48 -0400

    feat: define structure of commonMain/Puzzle.kt

---
+++ src/commonMain/kotlin/Puzzle.kt
@@ -0,0 +1,11 @@
+data class Puzzle(
+    val edges: List<String>,
+)
+
+@Throws(NotImplementedError::class)
+fun solve(
+    puzzle: Puzzle,
+    maxWords: Int,
+): Sequence<Sequence<String>> {
+    throw NotImplementedError()
+}
