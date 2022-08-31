b44d74c370aaa013f081d9e47e479e4287099a48
Guthrie McAfee Armstrong <git@gmarmstrong.dev>
2022-08-31 17:20:40 -0400

    test: add PuzzleTests.testEdgeDefinitionFlexible

--- src/commonTest/kotlin/PuzzleTests.kt
+++ src/commonTest/kotlin/PuzzleTests.kt
@@ -1,9 +1,20 @@
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class PuzzleTests {
     @Test
     fun testTautology() {
         assertTrue { true }
+    }
+
+    /**
+     * Test that comma-separated seeds are equivalent to string-list seeds.
+     */
+    @Test
+    fun testEdgeDefinitionFlexible() {
+        val seedA = "RME,WCL,KGT,IPA"
+        val seedB = listOf("RME", "WCL", "KGT", "IPA")
+        assertEquals(Puzzle(seedA), Puzzle(seedB))
     }
 }
