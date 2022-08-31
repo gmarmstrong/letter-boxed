10dac3149352fd5c18572cdb6f342ceb0f6cf9b4
Guthrie McAfee Armstrong <git@gmarmstrong.dev>
2022-08-31 17:20:20 -0400

    feat: overload Puzzle constructor for CSV-like inputs

--- src/commonMain/kotlin/Puzzle.kt
+++ src/commonMain/kotlin/Puzzle.kt
@@ -1,6 +1,11 @@
 data class Puzzle(
     val edges: List<String>,
-)
+) {
+    /**
+     * Constructs a puzzle from a comma-separated list of edges.
+     */
+    constructor(input: String) : this(input.split(","))
+}
 
 @Throws(NotImplementedError::class)
 fun solve(
