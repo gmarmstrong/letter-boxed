42e371b33a69c46a570ba070f3e7a628f7e5a446
Guthrie McAfee Armstrong <git@gmarmstrong.dev>
2022-08-31 23:47:40 -0400

    fix(jvm): resolve path to words.txt resource, satisfies returnWordsFromFile test

--- src/jvmMain/kotlin/WordsSourceImpl.kt
+++ src/jvmMain/kotlin/WordsSourceImpl.kt
@@ -1,7 +1,12 @@
 import java.io.File
 
 object WordsSourceImpl: WordsSource() {
+    private const val WORDS_FILE_NAME = "/words.txt"
+
     override fun getWords(): MutableSet<String> {
-        return File("words.txt").readLines().toMutableSet()
+        val wordsResourceURL = WordsSourceImpl::class.java.getResource(WORDS_FILE_NAME)
+            ?: throw IllegalStateException("Words file not found")
+        val wordsFilePath = wordsResourceURL.file
+        return File(wordsFilePath).readLines().toMutableSet()
     }
 }
