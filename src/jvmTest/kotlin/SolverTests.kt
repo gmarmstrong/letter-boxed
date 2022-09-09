a2ff57d15a23b9d20af7e5626bffe88d3b1a1253
Guthrie McAfee Armstrong <git@gmarmstrong.dev>
2022-09-08 23:02:32 -0400

    test: break up SolverTests and skip unnecessary re-solves

--- src/jvmTest/kotlin/SolverTests.kt
+++ src/jvmTest/kotlin/SolverTests.kt
@@ -1,47 +1,44 @@
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class SolverTests {
-    private val seed = "QRE,LOU,IAY,CNG"
-    private val puzzle = Puzzle(seed)
-    private val abcSeed = "ABC,DEF,GHI,JKL"
-    private val abcPuzzle = Puzzle(abcSeed)
+    private val puzzle = Puzzle("QRE,LOU,IAY,CNG")
+    private val abcPuzzle = Puzzle("ABC,DEF,GHI,JKL")
+    private val qreSolution = listOf("uniquely", "Yogacara")
+    private val qreBadSolution = listOf("car", "rogue")
+    private val solver = Solver(WordsSourceImpl)
 
-    private lateinit var solver: Solver
-
-    @BeforeEach
-    fun setup() {
-        solver = Solver(WordsSourceImpl)
+    @Test
+    fun testSolvesQre() = with(solver) {
+        assertTrue(qreSolution solves puzzle, "QRE puzzle should be solved by $qreSolution")
     }
 
     @Test
-    fun testIsSolution() {
-        val qreSolution = listOf("uniquely", "Yogacara")
-        val qreBadSolution = listOf("car", "rogue")
-        with(solver) {
-            assertTrue(qreSolution solves puzzle, "QRE puzzle should be solved by $qreSolution")
-            assertFalse(qreBadSolution solves puzzle, "QRE puzzle should not be solved by $qreBadSolution")
-            assertFalse(qreSolution solves abcPuzzle, "ABC puzzle should have no solutions")
-        }
+    fun testNotSolvesQre() = with(solver) {
+        assertFalse(qreBadSolution solves puzzle, "QRE puzzle should not be solved by $qreBadSolution")
     }
 
     @Test
-    fun testSolverSolve() {
-        val solutions = solver.solve(puzzle)
-        assertTrue(solutions.isNotEmpty(), "No solutions found")
-        assertTrue(solutions.contains(listOf("uniquely", "Yogacara")))
+    fun testNotSolvesAbc() = with(solver) {
+        assertFalse(qreSolution solves abcPuzzle, "ABC puzzle should not be solved by $qreSolution")
+    }
+
+    @Test
+    fun testQreSolutions() {
+        val qreSolutions = solver.solve(puzzle)
+        assertTrue(qreSolutions.isNotEmpty(), "QRE puzzle should have solutions")
+        assertTrue(qreSolutions.contains(qreSolution), "QRE puzzle should have solution $qreSolution")
         with(solver) {
-            solutions.forEach {
-                assertTrue(it solves puzzle, "Puzzle should be solved by $it")
+            qreSolutions.forEach {
+                assertTrue(it solves puzzle, "QRE puzzle should be solved by $it")
             }
         }
     }
 
     @Test
     fun testSolverSolveImpossible() {
-        val solutions = solver.solve(abcPuzzle)
-        assertTrue(solutions.isEmpty(), "The ABC puzzle should have no solutions")
+        val abcSolutions = solver.solve(abcPuzzle)
+        assertTrue(abcSolutions.isEmpty(), "ABC puzzle should have no solutions")
     }
 }
