e1df947465c71aa0b5b38602b1b16dded08ff4ff
Guthrie McAfee Armstrong <git@gmarmstrong.dev>
2022-09-08 22:07:55 -0400

    test: use setup function in SolverTests

--- src/jvmTest/kotlin/SolverTests.kt
+++ src/jvmTest/kotlin/SolverTests.kt
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class SolverTests {
@@ -8,28 +9,31 @@ class SolverTests {
     private val abcSeed = "ABC,DEF,GHI,JKL"
     private val abcPuzzle = Puzzle(abcSeed)
 
+    private lateinit var solver: Solver
+
+    @BeforeEach
+    fun setup() {
+        solver = Solver(WordsSourceImpl)
+    }
+
     @Test
     fun testIsSolution() {
-        val solver = Solver(WordsSourceImpl)
         assertTrue(solver.isSolution(listOf("uniquely", "Yogacara"), puzzle))
         assertFalse(solver.isSolution(listOf("car", "rogue"), puzzle))
     }
 
     @Test
     fun testSolverSolve() {
-        val solver = Solver(WordsSourceImpl)
         val solutions = solver.solve(puzzle)
         assertTrue(solutions.isNotEmpty(), "No solutions found")
         assertTrue(solutions.contains(listOf("uniquely", "Yogacara")))
         for (solution in solutions) {
             assertTrue(solver.isSolution(solution, puzzle), "Not all of these solutions are valid")
         }
-        println(solutions.joinToString("\n"))
     }
 
     @Test
     fun testSolverSolveImpossible() {
-        val solver = Solver(WordsSourceImpl)
         val solutions = solver.solve(abcPuzzle)
         assertTrue(solutions.isEmpty(), "The ABC puzzle should have no solutions")
     }
