2e4d61312c7217046bb4a108bd4266fc7addb2d8
Guthrie McAfee Armstrong <git@gmarmstrong.dev>
2022-09-09 08:16:08 -0400

    docs: document primary Puzzle constructor

--- src/main/kotlin/Puzzle.kt
+++ src/main/kotlin/Puzzle.kt
@@ -6,10 +6,13 @@ data class Puzzle(val edges: Set<Set<Char>>) {
     val letters: Set<Char>
         get() = edges.flatten().toSet()
 
+    /**
+     * Constructs a puzzle from a set of 4 edges (sets of 3 uppercase ASCII characters).
+     */
     init {
         require(edges.size == 4) { "Puzzle must have exactly 4 unique edges" }
         require(edges.all { it.size == 3 }) { "Each edge must be exactly 3 unique characters" }
-        require(edges.all { edge -> edge.all { it in 'A'..'Z' } }) { "Each edge must contain only uppercase Latin letters" }
+        require(edges.all { edge -> edge.all { it in 'A'..'Z' } }) { "Each edge must contain only uppercase ASCII letters" }
         require(edges.flatten().toSet().size == 12) { "Puzzle must consist of exactly 12 unique characters" }
     }
 
