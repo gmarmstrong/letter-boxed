2a45600a7afbe1dfdeca6a83aa5fe6257d372f13
Guthrie McAfee Armstrong <git@gmarmstrong.dev>
2022-09-09 08:41:29 -0400

    test: functional tests for invalid inputs

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
@@ -7,6 +9,7 @@ import java.io.PrintStream
  * Functional tests for the [main] entry point.
  */
 class MainTest {
+    /* Puzzles and their expected outputs. */
     private val expectations = mapOf(
         arrayOf("RME,WCL,KGT,IPA") to "[wigwam, marketplace]",
         arrayOf("LWC,GTK,ERM,PIA") to "[wigwam, marketplace]",
@@ -20,17 +23,51 @@ class MainTest {
             [virilize, Ethiopian]
             [heliozoan, nonrepetitive]
             [perihelion, novelization]
-        """.trimIndent()
+        """.trimIndent(),
     )
+
+    private lateinit var out: ByteArrayOutputStream
+
+    @BeforeEach
+    fun setUp() {
+        out = ByteArrayOutputStream()
+        System.setOut(PrintStream(out))
+    }
 
     @Test
     fun mainOutputTest() {
-        val out = ByteArrayOutputStream()
-        System.setOut(PrintStream(out))
         for ((input, expected) in expectations) {
             main(input)
-            assertEquals(expected, out.toString().trim())
+            val output = out.toString().trim()
+            assertEquals(expected, output, "Unexpected output for input $input (got $output, expected $expected)")
             out.reset()
         }
+    }
+
+    @Test
+    fun mainDuplicateEdgeTest() {
+        assertThrows(
+            IllegalArgumentException::class.java,
+            { main(arrayOf("ABC,ABC,ABC,ABC")) },
+            "Expected error for duplicate edges, got ${out.toString().trim()} instead"
+        )
+    }
+
+    @Test
+    fun mainTooManyEdgesTest() {
+        assertThrows(
+            IllegalArgumentException::class.java,
+            { main(arrayOf("ABC,DEF,GHI,JKL,MNO")) },
+            "Expected error for too many edges, got ${out.toString().trim()} instead"
+        )
+    }
+
+    @Test
+    fun mainTooFewEdgesTest() {
+        assertThrows(
+            IllegalArgumentException::class.java,
+            { main(arrayOf("ABC,DEF,GHI")) },
+            "Expected error for too few edges, got ${out.toString().trim()} instead"
+        )
     }
 }
