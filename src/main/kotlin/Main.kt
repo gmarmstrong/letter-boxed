b0239761f6af03af604dad77244522257128ad4f
Guthrie McAfee Armstrong <git@gmarmstrong.dev>
2022-09-08 23:34:39 -0400

    build!: convert from multiplatform to CLI

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val solver = Solver(WordsSourceImpl)
+    val seed = args[0]
+    val puzzle = Puzzle(seed)
+    val solutions = solver.solve(puzzle)
+    solutions.forEach(::println)
+}
